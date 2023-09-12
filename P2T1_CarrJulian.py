63181b40cb9888de105dd349f760b1a1f367fbd1
carrj5377 <143745660+carrj5377@users.noreply.github.com>
2023-09-12 13:54:53 -0400

    Add files via upload
    First version of the pokeman graph

---
+++ P2T1_CarrJulian.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+# Collect data
+print("Enter poekman data:")
+print("Day 1:", end="")
+day1 = int(input())
+print("Day 2:", end="")
+day2 = int(input())
+print("Day 3:", end="")
+day3 = int(input())
+
+# Put the data in a list
+data = [day1, day2, day3]
+
+MX = max(data)
+print(MX)
+
+MN = min(data)
+
+
+
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3], data)
+plt.title("Pokeman Data")
+plt.xlabel('day #')
+plt.ylabel('pokemans collected')
+plt.show()
